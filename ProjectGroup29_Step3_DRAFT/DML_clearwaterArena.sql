85d63af6af7a411a9132a92a89cb46a27729eb39
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-07-24 12:07:10 -0700

    Update DML_clearwaterArena.sql

--- ProjectGroup29_Step3_DRAFT/DML_clearwaterArena.sql
+++ ProjectGroup29_Step3_DRAFT/DML_clearwaterArena.sql
@@ -4,43 +4,43 @@
 -- functionalities listed in the Project Specs.
 
 -- READ OPERATIONS
--- concerts
--- get all concerts
-SELECT * FROM Concerts;
-
 -- artists
 -- get all artists
 SELECT * FROM Artists;
 
--- tickets
--- get all tickets
-SELECT * FROM Tickets;
-
-
--- concerts_has_artists
--- get all concerts and the corresponding artists
-SELECT Concerts.concertID, Artists.artistID as artist from Concerts
-Inner JOIN Artists ON Concerts.concertID = Artists.artistID;
-
--- concerts_has_employees
--- get all concerts and the corresponding employees
-SELECT Concerts.concertID, Employees.employeeID as employee from Concerts
-Inner JOIN Employees ON Concerts.concertID = Employees.employeeID;
-
 -- employees
 -- get all employees
-SELECT Employees.employeeID, Employees.firstName, Employees.lastName, Employees.role, Employees.email, Employees.phoneNumber FROM Employees;
+SELECT * FROM Employees;
 
 -- fans
 -- get all fans
-SELECT Fans.fanID, Fans.firstName, Fans.lastName, Fans.email, Fans.phoneNumber, Fans.streetAddress, Fans.city, Fans.state, Fans.zipCode FROM Fans;
+SELECT * FROM Fans;
 
+
+-- concerts_has_artists
+-- get all concerts_has_artists
+SELECT Concerts.concertID, Artists.artistID AS artist FROM Concerts
+INNER JOIN Artists ON Concerts.concertID = Artists.artistID;
+
+-- concerts_has_employees
+-- get all concerts_has_employees
+SELECT Concerts.concertID, Employees.employeeID AS employee FROM Concerts
+INNER JOIN Employees ON Concerts.concertID = Employees.employeeID;
+
+
+-- concerts
+-- get all concerts and the corresponding artists and employees
+SELECT Concerts.concertID, Concerts.concertDate, Concerts.numberOfTickets, Artists.name AS artist FROM Concerts
+SELECT Concerts.concertID, Concerts.concertDate, Concerts.numberOfTickets, Employees.firstName, Employees.lastName AS employee FROM Concerts
+INNER JOIN Artists ON Concerts.concertID = Artists.artistID
+INNER JOIN Employees ON Concerts.concertID = Employees.employeeID;
 
 -- tickets
--- get all concerts and fans within the tickets
-SELECT Tickets.ticketID, Fans.fanID, Concerts.concertID as ticketID FROM Tickets
-INNER JOIN Fans on Tickets.ticketID = Fans.fanID
-INNER JOIN Concerts on Tickets.ticketID = Concerts.concertID;
+-- get all tickets and the corresponding artists and fans
+SELECT Tickets.ticketID, Tickets.concertID, Artists.name AS artist FROM Tickets
+SELECT Tickets.ticketID, Tickets.fanID, Fans.firstName, Fans.lastName AS fan FROM Tickets
+INNER JOIN Artists ON Tickets.concertID = Artists.artistID
+INNER JOIN Fans ON Tickets.ticketID = Fans.fanID;
 
 -- CREATE OPERATIONS
 -- create a new Concert
@@ -53,7 +53,7 @@ INSERT INTO Artists (Artists.name, Artists.phoneNumber) VALUES (:nameInput, :pho
 INSERT INTO Tickets (Tickets.concertID, Tickets.fanID) VALUES (:concertIDInput, :fanIDInput);
 
 -- create a new Employee
-INSERT INTO Employees (Employees.firstName, Employees.lastName, Employees.role, Employees.email, Employees.phoneNumber) VALUES (:firstNameInput, :lastNameInput, :roleFromDropDown, :emailInput, :phoneNumberInput);
+INSERT INTO Employees (Employees.firstName, Employees.lastName, Employees.role, Employees.email, Employees.phoneNumber) VALUES (:firstNameInput, :lastNameInput, :roleInput, :emailInput, :phoneNumberInput);
 
 -- create a new Fan
 INSERT INTO Fans (Fans.firstName, Fans.lastName, Fans.email, Fans.phoneNumber, Fans.streetAddress, Fans.city, Fans.state, Fans.zipCode) VALUES (:firstNameInput, :lastNameInput, :emailInput, :phoneNumberInput, :streetAddressInput, :cityInput, :stateInput, :zipCodeInput);
@@ -67,16 +67,35 @@ UPDATE Fans
     SET firstName = :firstNameInput, lastName = :lastNameInput, email = :emailInput, streetAddress = :streetAddressInput, city = :cityInput, state = :stateInput, zipCode = :zipCodeInput
     WHERE fanID = :selectedFanID
     
--- UPDATE OPERATIONS
 -- update an employee
 UPDATE Employees
-    SET firstName = :firstNameInput, lastName = :lastNameInput, role = :roleInput, phoneNumber = :phoneNumberInput
+    SET firstName = :firstNameInput, lastName = :lastNameInput, role = :roleInput, email = :emailInput, phoneNumber = :phoneNumberInput
     WHERE employeeID = :selectedEmployeeID
+    
+-- update an artist
+UPDATE Artists
+    SET name = :nameInput, phoneNumber = :phoneNumberInput
+    WHERE artistID = :selectedArtistID
 
 -- DELETE OPERATIONS
 -- delete a ticket from a concert
 DELETE FROM Tickets WHERE
     (
-        SELECT Concerts.concertID FROM Tickets
+        SELECT Tickets.concertID FROM Tickets
         WHERE concertID = :selectedConcert AND ticketID = :selectedTicket
     )
+
+-- delete an employee from a concert
+DELETE FROM Employees WHERE
+    (
+        SELECT Employees.employeeID FROM Concert_has_Employees
+        WHERE concertID = :selectedConcert AND employeeID = :selectedEmployee
+    )
+ 
+-- delete an artist from a concert
+DELETE FROM Artists WHERE
+    (
+        SELECT Artists.artistID FROM Concert_has_Artists
+        WHERE concertID = :selectedConcert AND artistID = :selectedArtist
+    )
+
