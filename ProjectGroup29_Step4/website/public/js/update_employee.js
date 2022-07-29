efbf74ef0abff059df1ecc7c62ba566f45b8a835
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-07-29 11:35:34 -0700

    Create update_employee.js

---
+++ ProjectGroup29_Step4/website/public/js/update_employee.js
@@ -0,0 +1,69 @@
+// Get the objects we need to modify
+let updateEmployeeForm = document.getElementById('update-employee-form-ajax');
+
+// Modify the objects we need
+updateEmployeeForm.addEventListener("submit", function (e) {
+   
+    // Prevent the form from submitting
+    e.preventDefault();
+
+    // Get form fields we need to get data from
+    let inputFullName = document.getElementById("mySelect");
+    let inputRole = document.getElementById("input-role-update");
+
+    // Get the values from the form fields
+    let fullNameValue = inputFullName.value;
+    let roleValue = inputRole.value;
+
+
+    // Put our data we want to send in a javascript object
+    let data = {
+        fullname: fullNameValue,
+        role: roleValue,
+    }
+    
+    // Setup our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("PUT", "/put-employee-ajax", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    // Tell our AJAX request how to resolve
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+            // Add the new data to the table
+            updateRow(xhttp.response, fullNameValue);
+
+        }
+        else if (xhttp.readyState == 4 && xhttp.status != 200) {
+            console.log("There was an error with the input.")
+        }
+    }
+
+    // Send the request and wait for the response
+    xhttp.send(JSON.stringify(data));
+
+})
+
+
+function updateRow(data, empID){
+    let parsedData = JSON.parse(data);
+    
+    let table = document.getElementById("employee-table");
+
+    for (let i = 0, row; row = table.rows[i]; i++) {
+       //iterate through rows
+       //rows would be accessed using the "row" variable assigned in the for loop
+       if (table.rows[i].getAttribute("data-value") == empID) {
+
+            // Get the location of the row where we found the matching person ID
+            let updateRowIndex = table.getElementsByTagName("tr")[i];
+
+            // Get td of homeworld value
+            let td = updateRowIndex.getElementsByTagName("td")[3];
+
+            // Reassign homeworld to our value we updated to
+            td.innerHTML = parsedData[0].name; 
+       }
+    }
+}
