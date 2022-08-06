4f48bedd717f9b83370b2a9366eed2fab3a5d68b
Mallory Huston <55804897+MalloryHuston@users.noreply.github.com>
2022-08-06 11:00:11 -0700

    Update DML_clearwaterArena.sql

--- ProjectGroup29_Step5/DML_clearwaterArena.sql
+++ ProjectGroup29_Step5/DML_clearwaterArena.sql
@@ -1,7 +1,7 @@
 -- READ OPERATIONS
--- ticket_types
--- get all types of tickets
-SELECT Ticket_Types.ticket_typeID FROM Ticket_Types;
+-- categories
+-- get all types ticket categories
+SELECT Categories.categoryID FROM Categories;
 
 -- artists
 -- get all artists
@@ -16,47 +16,41 @@ SELECT * FROM Employees;
 SELECT * FROM Fans;
 
 -- concerts
--- get all concerts and all artists and employees within the concert
-SELECT Concerts.concertID, Concerts.concertDate, Concerts.numberOfTickets, Artists.name AS artist, Employees.lastName AS employee FROM Concerts
-INNER JOIN Concerts_Artists on Concerts_Artists.concertID = Concerts.concertID
-INNER JOIN Artists on Concerts_Artists.artistID = Artists.artistID
+-- get all concerts and all employees within the concert
+SELECT Concerts.concertID, Concerts.concertDate, Concerts.numberOfTickets, Concerts.name, Concerts.description, Employees.lastName FROM Concerts
 INNER JOIN Concerts_Employees on Concerts_Employees.concertID = Concerts.concertID
 INNER JOIN Employees on Concerts_Employees.employeeID = Employees.employeeID;
 
 -- tickets
--- get all tickets and the corresponding concert, fan, and type of ticket
-SELECT Tickets.ticketID, Tickets.duration, Concerts.concertDate AS concert, Fans.email AS fan, Ticket_Types.ticket_typeID AS ticket_type FROM Tickets
+-- get all tickets and the corresponding concert, fan, and type of ticket category
+SELECT Tickets.ticketID, Tickets.duration, Concerts.concertDate AS concert, Fans.email AS fan, Categories.categoryID AS category FROM Tickets
 INNER JOIN Concerts ON Concerts.concertID = Tickets.concertID
 INNER JOIN Fans ON Fans.fanID = Tickets.fanID
-INNER JOIN Ticket_Types ON Ticket_Types.ticket_typeID = Tickets.ticket_typeID;
+INNER JOIN Categories ON Categories.categoryID = Tickets.categoryID;
 
--- filter by concert date
-SELECT Concerts.concertDate, Concerts.numberOfTickets, Artists.name FROM Concerts_Artists
-SELECT Concerts.concertDate, Concerts.numberOfTickets, Employees.lastName FROM Concerts_Employees
-WHERE Concerts.concertDate = :concertdatefilter;
+-- filter by concert name
+SELECT Concerts.name, Concerts.concertDate, Concerts.description, Artists.name FROM Concerts_Employees
+WHERE Concerts.name = :concertnamefilter;
 
 
 -- CREATE OPERATIONS
--- create a new type of Ticket
-INSERT INTO Ticket_Types (Ticket_Types.ticket_typeID) VALUES (:ticket_typeIDInput);
+-- create a new type of Ticket Category
+INSERT INTO Categories (Categories.categoryID) VALUES (:categoryIDInput);
 
 -- create a new Concert
-INSERT INTO Concerts (Concerts.concertDate, Concerts.numberOfTickets) VALUES (:concertDateInput, :numberOfTicketsInput);
+INSERT INTO Concerts (Concerts.concertDate, Concerts.numberOfTickets, Concerts.name, Concerts.description, Concerts.artistID) VALUES (:concertDateInput, :numberOfTicketsInput, :nameInput, :descriptionInput, :artistIDFromDropDown);
 
 -- create a new Artist
-INSERT INTO Artists (Artists.name, Artists.phoneNumber) VALUES (:nameInput, :phoneNumberInput);
+INSERT INTO Artists (Artists.name, Artists.bio) VALUES (:nameInput, :bioInput);
 
 -- create a new Ticket
-INSERT INTO Tickets (Tickets.duration, Tickets.concertID, Tickets.fanID, Tickets.ticket_typeID) VALUES (:durationInput, :concertIDFromDropDown, :fanIDFromDropDown, :ticket_typeIDFromDropDown);
+INSERT INTO Tickets (Tickets.duration, Tickets.concertID, Tickets.fanID, Tickets.categoryID) VALUES (:durationInput, :concertIDFromDropDown, :fanIDFromDropDown, :categoryIDFromDropDown);
 
 -- create a new Employee
 INSERT INTO Employees (Employees.firstName, Employees.lastName, Employees.role, Employees.email, Employees.phoneNumber) VALUES (:firstNameInput, :lastNameInput, :roleInput, :emailInput, :phoneNumberInput);
 
 -- create a new Fan
 INSERT INTO Fans (Fans.firstName, Fans.lastName, Fans.email, Fans.phoneNumber, Fans.streetAddress, Fans.city, Fans.state, Fans.zipCode) VALUES (:firstNameInput, :lastNameInput, :emailInput, :phoneNumberInput, :streetAddressInput, :cityInput, :stateInput, :zipCodeInput);
-
--- add an artist to a Concert
-INSERT INTO Concerts_Artists (Concerts_Artists.concertID, Concerts_Artists.artistID) VALUES (:concertInput, :artistInput);
 
 -- add an employee to a Concert
 INSERT INTO Concerts_Employees (Concerts_Employees.concertID, Concerts_Employees.employeeID) VALUES (:concertInput, :employeeInput);
@@ -72,16 +66,13 @@ UPDATE Fans
 -- DELETE OPERATIONS
 -- delete an employee from a concert
 DELETE FROM Concerts_Employees WHERE concertID = :selectedConcert AND employeeID = :selectedEmployee;
- 
--- delete an artist from a concert
-DELETE FROM Concerts_Artists WHERE concertID = :selectedConcert AND artistID = :selectedArtist;
 
--- delete an entire concert from the Concerts table, all associated entries in both the Concerts_Artists and Concerts_Employees tables should be deleted by CASCADE
+-- delete an entire concert from the Concerts table, all associated entries in the Concerts_Employees table should be deleted by CASCADE
 DELETE FROM Concerts
 WHERE concertID = :selectedConcert;
 
 
 -- set a category in a ticket to be null
 UPDATE Tickets
-    SET Tickets.ticket_typeID = NULL
+    SET Tickets.categoryID = NULL
     WHERE Tickets.ticketID = :selectedTicketID;
